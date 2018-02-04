117b9e479bd2ac0394bf270ff6d6b4a8f1c91887
Alsk Jet <swm321@gmail.com>
2018-02-04 16:35:24 -0500

    Added extention checking & using Set instead of list for efficiency

--- cleanmachine/src/player/music/io/SongLoader.java
+++ cleanmachine/src/player/music/io/SongLoader.java
@@ -5,11 +5,13 @@ import player.music.SongDefinition;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SongLoader implements FileLoader<SongDefinition> {
 
-    private static final List<String> EXTENTIONS = Arrays.asList(".mp4", ".mp3", ".wav");
+    private static final Set<String> ACCEPTED_EXTENTIONS = new HashSet<>(Arrays.asList("mp4", "mp3", "wav"));
     private static final String DEFAULT_LOCATION = CACHE_LOCATION + "songs/";
 
 
@@ -18,11 +20,21 @@ public class SongLoader implements FileLoader<SongDefinition> {
      */
     @Override
     public SongDefinition load(String fileName) throws IOException {
+
+        if(!isAcceptableExtention(fileName)) {
+            throw new IllegalArgumentException("Illegal extention - it should be filtered before it gets to this point!");
+        }
+
         final File file = new File(DEFAULT_LOCATION + fileName);
 
         //load data via song library
 
         return new SongDefinition("", "", 0);
+    }
+
+    private boolean isAcceptableExtention(final String fileName) {
+        final String extention = fileName.substring(fileName.lastIndexOf("."));
+        return ACCEPTED_EXTENTIONS.contains(fileName);
     }
 
 }
